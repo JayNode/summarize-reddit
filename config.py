3f2b877c2754f28f1ac55304fdbaeef5bf13c40a
Jared Immerman <jimmerman819@gmail.com>
2024-07-25 10:31:13 -0700

    Runs, no resulting error, cannot find output ~Mac

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 """Required constants for the Reddit API."""
 
 # The following constants are used by the bot.
-REDDIT_USERNAME = ""
-REDDIT_PASSWORD = ""
+REDDIT_USERNAME = "Summarizer-Bot"
+REDDIT_PASSWORD = "20Bot24!"
 
-APP_ID = ""
-APP_SECRET = ""
-USER_AGENT = ""
+APP_ID = "3AFCZYbi69BlO59nbt8SaQ"
+APP_SECRET = "zLdRUQkaabXfEm3ptq2ai2Y8Dcd-Bw"
+USER_AGENT = "Python:SummarizerReddit:v1.0 (by /u/Summarizer-Bot)"
 
 SUBREDDITS = ["mexico"]
 
